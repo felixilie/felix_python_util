c1ec3a82d91aaeacc8a41872e94145ae97150004
felixilie <41553601+felixilie@users.noreply.github.com>
2020-10-21 10:29:58 -0400

    url fixed

--- setup.py
+++ setup.py
@@ -8,7 +8,7 @@ setuptools.setup(
     description="A small example package",
     long_description='carp',
     long_description_content_type="text/markdown",
-    url="https://github.com/felixilie/felix_python_util",
+    url="",
     packages=setuptools.find_packages(),
     classifiers=[
         "Programming Language :: Python :: 3",
